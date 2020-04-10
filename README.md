55e7221db608177dffd62182977ed067de7fafac
Moeyyad Qureshi <moeyyadqureshi@gmail.com>
2020-04-09 20:12:57 -0400

    added readme, code reorganization

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# AdvTicTacToe
+# Advanced Tic Tac Toe
+Simple Tic-Tac-Toe game that runs in terminal. 
+
+What makes it advanced? Beginners to programming often try to build Tic-Tac-Toe as one of their first learning projects. This is something I did 4 years ago while I learning Python. I decided to rebuild this game and see how different my code looks now vs when I first began.
--- game.py
+++ game.py
@@ -97,6 +97,28 @@ class Game:
 
         self.current = self.p1
         self.message = "Have a great game!"
+    
+    def _next_player(self) -> Player:
+        if self.current is self.p1:
+            return self.p2
+        return self.p1
+    
+    def _display(self) -> None:
+        os.system('clear')
+        print(dedent(
+            f""" 
+            ============ SCOREBOARD ============
+            {self.p1.name}: {self.p1.score} wins | {self.p2.name}: {self.p2.score} wins
+            ====================================
+
+            MESSAGE: {self.message or 'Keep the pressure up!'}
+            """
+        ))
+
+        print(f"GAME #{self.game_number}", end="")
+        print(self.board)
+
+        self.message = None
 
     def _restart(self) -> bool:
         self.current = self._next_player()
@@ -121,29 +143,7 @@ class Game:
         self.message = "New game, let's see who wins this one!"
 
         return input("Enter 999 to play again: ") == "999"
-    
-    def _next_player(self) -> Player:
-        if self.current is self.p1:
-            return self.p2
-        return self.p1
 
-    def _display(self) -> None:
-        os.system('clear')
-        print(dedent(
-            f""" 
-            ============ SCOREBOARD ============
-            {self.p1.name}: {self.p1.score} wins | {self.p2.name}: {self.p2.score} wins
-            ====================================
-
-            MESSAGE: {self.message or 'Keep the pressure up!'}
-            """
-        ))
-
-        print(f"GAME #{self.game_number}", end="")
-        print(self.board)
-
-        self.message = None
-        
     def run(self) -> None:
         os.system('clear')
         print("Welcome to Tic-Tac-Toe. You already know the rules")
@@ -171,6 +171,7 @@ class Game:
                 break
             
         print("\nThanks for playing!\n")
+
 
 if __name__ == '__main__':
     Game().run()
